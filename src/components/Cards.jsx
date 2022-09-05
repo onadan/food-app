96f9dbf92d6501bc89ed9fa36bf44ea47dbe5202
onadan <onadan45@gmail.com>
2022-09-05 14:28:28 +0100

    cart router added

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -19,17 +19,17 @@ export const LandingPageCard = ({ image, name, desc }) => {
 export const FoodCard = ({ image, name, desc, price }) => {
   return (
     <div className="border rounded-lg">
-      <div className="flex flex-col items-center my-4 gap-4 flex-wrap w-[20rem] p-4">
+      <div className="flex flex-col items-center mt-4 gap-3 flex-wrap w-[15rem] h-[19rem] p-4 relative">
         <div className="w-[5.6rem] h-[5.6rem] bg-slate-400 rounded-full">
           <img src={image} alt="" />
         </div>
         <div className="font-semibold text-lg">{name}</div>
-        <div className="text-xl text-black max-h-14 text-ellipsis overflow-hidden">
+        <div className="text-xs text-black">
           {desc}
         </div>
-        <div className="flex justify-between items-center w-full">
+        <div className="flex justify-around items-center w-full absolute bottom-0 ">
           <p className="font-bold text-sm text-[#00302e]">{price}</p>
-          <div className="p-4 font-medium text-sm bg-[#00302e] text-[#06e775]">
+          <div className="p-4 font-medium text-sm text-[#06e775]">
             Add to Cart
           </div>
         </div>
--- src/pages/dashboard/Cart.js
+++ src/pages/dashboard/Cart.js
@@ -3,13 +3,15 @@ import { Link } from "react-router-dom";
 const Cart = () => {
   return (
     <div className="relative">
-      <Link to="../../dashboard">
-        <div className="bg-[#C4C4C46B] h-screen w-screen"></div>
+      <Link to="../../dashboard" className="hidden sm:block">
+        <div className="bg-[#1b1b1bc4] h-screen w-screen"></div>
       </Link>
 
-      <div className="absolute top-0 right-0 w-[30rem] bg-white min-h-screen">
-        <h1 className="font-bold text-3xl">Cart</h1>
-        <Link to="../../dashboard">Go back</Link>
+      <div className="sm:absolute sm:top-0 sm:right-0 sm:w-[30rem] bg-white min-h-screen w-screen">
+        <div className="px-4 pt-4">
+          <h1 className="font-bold text-3xl">Cart</h1>
+          <Link to="../../dashboard">Go back</Link>
+        </div>
       </div>
     </div>
   );
